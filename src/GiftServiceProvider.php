bc21fffe3e7730c62312b32ecfc7ea61be4d6bbe
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-02-26 15:09:12 +0600

    code formatted and optimized

--- src/GiftServiceProvider.php
+++ src/GiftServiceProvider.php
@@ -16,7 +16,7 @@ class GiftServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__.'/../config/gift.php', 'fintech.gift'
+            __DIR__ . '/../config/gift.php', 'fintech.gift'
         );
 
         $this->app->register(RouteServiceProvider::class);
@@ -29,21 +29,21 @@ class GiftServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->publishes([
-            __DIR__.'/../config/gift.php' => config_path('fintech/gift.php'),
+            __DIR__ . '/../config/gift.php' => config_path('fintech/gift.php'),
         ]);
 
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 
-        $this->loadTranslationsFrom(__DIR__.'/../lang', 'gift');
+        $this->loadTranslationsFrom(__DIR__ . '/../lang', 'gift');
 
         $this->publishes([
-            __DIR__.'/../lang' => $this->app->langPath('vendor/gift'),
+            __DIR__ . '/../lang' => $this->app->langPath('vendor/gift'),
         ]);
 
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'gift');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'gift');
 
         $this->publishes([
-            __DIR__.'/../resources/views' => resource_path('views/vendor/gift'),
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/gift'),
         ]);
 
         if ($this->app->runningInConsole()) {
