d6bae244f4a025914f3245e5f6b6b37af958cd3f
hafijul233 <hafijul233@users.noreply.github.com>
2024-05-07 21:04:15 +0000

    Fix styling

--- src/GiftServiceProvider.php
+++ src/GiftServiceProvider.php
@@ -53,7 +53,7 @@ class GiftServiceProvider extends ServiceProvider
 
         if ($this->app->runningInConsole()) {
             $this->commands([
-                InstallCommand::class
+                InstallCommand::class,
             ]);
         }
     }
